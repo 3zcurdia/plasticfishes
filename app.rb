67e9e360e3ba7ef11dde305a8c38ad5934208cb3
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2018-04-04 23:49:03 -0500

    remove format on api link

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ def hash_for_fish(id)
     id: id,
     name: id.tr('-', ' ').capitalize,
     text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    api_url: "#{request.base_url}/api/fishes/#{id}.json",
+    api_url: "#{request.base_url}/api/fishes/#{id}",
     web_url: "#{request.base_url}/fishes/#{id}",
     image_url: "#{request.base_url}/#{id}.png"
   }
