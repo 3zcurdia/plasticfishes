92ca8715305364c82392e24656e5ce46ffe874af
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2020-12-26 10:41:10 -0600

    Encapsulate logic into store singleton class

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@
 require 'rubygems'
 require 'rack/cache'
 require 'sinatra'
+require 'singleton'
 require 'json'
 
 # Assets logic to read assets filanmes and filepaths
@@ -14,7 +15,7 @@ module Assets
   end
 
   def file_paths
-    Dir[File.dirname(__FILE__) + '/public/*.png']
+    Dir["#{File.dirname(__FILE__)}/public/*.png"]
   end
 
   def sanitize_filepath(path)
@@ -22,21 +23,69 @@ module Assets
   end
 end
 
+# Store generasete a s simulated database store
+class Store
+  include Singleton
+
+  def initialize
+    @fishes = Assets.filenames.map do |filename|
+      [filename, build(filename)]
+    end.to_h.freeze
+  end
+
+  def all
+    fishes.values
+  end
+
+  def find(id)
+    fishes[id]
+  end
+
+  def include?(id)
+    fishes.key?(id)
+  end
+
+  def random_key
+    fishes.keys.sample
+  end
+
+  def random
+    find(random_key)
+  end
+
+  private
+
+  attr_reader :fishes
+
+  def build(key)
+    {
+      id: key,
+      name: key.tr('-', ' ').capitalize,
+      uuid: SecureRandom.uuid,
+      number: SecureRandom.hex(3).to_i(16),
+      bigint: SecureRandom.hex(32).to_i(16),
+      text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      api_path: "/api/fishes/#{key}",
+      web_path: "/fishes/#{key}",
+      image_path: "/#{key}.png",
+      created_at: Time.now.utc.iso8601
+    }
+  end
+end
+
 # PlasticFishesApp: main app
 class PlasticFishesApp < Sinatra::Base
-  FISHES = Assets.filenames.freeze
-
   get '/' do
     cache_control :public, max_age: 36_000
     erb :index
   end
 
-  get '/fishes/random' do
-    redirect to("/fishes/#{FISHES.sample}")
-  end
-
-  get '/fishes/random.png' do
-    redirect to("/#{FISHES.sample}.png")
+  get '/fishes/random/?:format?' do
+    if params[:format] == '.png'
+      redirect to("/#{Store.instance.random_key}.png")
+    else
+      redirect to("/fishes/#{Store.instance.random_key}")
+    end
   end
 
   get '/fishes/:id' do
@@ -46,63 +95,22 @@ class PlasticFishesApp < Sinatra::Base
     erb :show
   end
 
-  post '/api/fishes/' do
+  get '/api/fishes/?' do
     cache_control :public, max_age: 36_000
     content_type :json
-    fish_for([:id]).merge(fish_params).to_json
+    Store.instance.all.to_json
   end
 
-  get '/api/fishes' do
+  get '/api/fishes/random/?' do
+    content_type :json
+    Store.instance.random.to_json
+  end
+
+  get '/api/fishes/:id/?' do
     cache_control :public, max_age: 36_000
     content_type :json
-    FISHES.map { |name| fish_for(name) }.to_json
-  end
+    halt(404) unless Store.instance.include?(params[:id])
 
-  get '/api/fishes/random' do
-    content_type :json
-    fish_for(FISHES.sample).to_json
-  end
-
-  get '/api/fishes/:id' do
-    cache_control :public, max_age: 36_000
-    content_type :json
-    halt(404) unless FISHES.include?(params[:id])
-    fish_for(params[:id]).to_json
-  end
-
-  put '/api/fishes/:id' do
-    cache_control :public, max_age: 36_000
-    content_type :json
-    halt(404) unless FISHES.include?(params[:id])
-    fis_params
-    fish_for(params[:id]).merge(fish_params).to_json
-  end
-
-  delete '/api/fishes/:id' do
-    FISHES.include?(params[:id]) ? halt(404) : halt(204)
-  end
-
-  private
-
-  def fish_params
-    sanitized = params[:fish]
-    sanitized.delete(:id)
-    sanitized
-  end
-
-  def fish_for(id)
-    {
-      id: id,
-      name: id.tr('-', ' ').capitalize,
-      uuid: SecureRandom.uuid,
-      number: SecureRandom.hex(3).to_i(16),
-      bigint: SecureRandom.hex.to_i(16),
-      text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      api_url: "#{request.base_url}/api/fishes/#{id}",
-      web_url: "#{request.base_url}/fishes/#{id}",
-      image_url: "#{request.base_url}/#{id}.png",
-      time_utc: Time.now.utc,
-      time_iso8601: Time.now.utc.iso8601
-    }
+    Store.instance.find(params[:id]).to_json
   end
 end
